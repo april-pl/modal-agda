8eaf8f92480c07d78f006b68fb4ca3c53e3b31ca
sowelilili <atune313@gmail.com>
2024-08-19 13:09:33 +0100

    latex agda

---
+++ latex.sh
@@ -0,0 +1,21 @@
+mkdir lagda
+
+function agda_latex {
+    cd $1
+    for f in src/*.agda; do
+        agda --latex $f
+    done
+    cd ..
+}
+
+function take_latex {
+    mv $1/latex lagda/$1
+}
+
+function do_it {
+    agda_latex $1
+    take_latex $1
+}
+
+do_it fitch
+do_it moggi
